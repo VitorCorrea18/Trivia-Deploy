03bc4551a730f25a3d5743b1dc1dc8ae60bb35c1
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-24 23:52:49 -0300

    Updates

---
+++ precache-manifest.b0c39e3370ce82ea85265ba41b811ff6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953055d833ab63d173c6fcd1d50b05d5",
+    "url": "/Trivia-Deploy/index.html"
+  },
+  {
+    "revision": "e33a1a42b4eb7dd2155d",
+    "url": "/Trivia-Deploy/static/css/main.6fa92101.chunk.css"
+  },
+  {
+    "revision": "b04dab46b61e64cbb65a",
+    "url": "/Trivia-Deploy/static/js/2.286f306d.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/Trivia-Deploy/static/js/2.286f306d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e33a1a42b4eb7dd2155d",
+    "url": "/Trivia-Deploy/static/js/main.235a2f3f.chunk.js"
+  },
+  {
+    "revision": "35148ebe3ede458804fb",
+    "url": "/Trivia-Deploy/static/js/runtime-main.bd091721.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/Trivia-Deploy/static/media/trivia.466d153e.png"
+  }
+]);
